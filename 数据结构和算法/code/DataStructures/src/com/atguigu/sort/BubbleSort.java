43979498e5389518b9763eba768286ab870139e4
dll_glb <dll_glb@123.com>
2019-12-07 21:15:50 +0800

    数据结构和算法 排序算法部分

--- 数据结构和算法/code/DataStructures/src/com/atguigu/sort/BubbleSort.java
+++ 数据结构和算法/code/DataStructures/src/com/atguigu/sort/BubbleSort.java
@@ -4,9 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 
-
-
-
 public class BubbleSort {
 
 	public static void main(String[] args) {
@@ -14,9 +11,16 @@ public class BubbleSort {
 //		
 //		System.out.println("排序前");
 //		System.out.println(Arrays.toString(arr));
+//		//为了容量理解，我们把冒泡排序的演变过程，给大家展示
+//		//测试冒泡排序
+////		bubbleSort1(arr);
+//		bubbleSort(arr);
+//		
+//		System.out.println("排序后");
+//		System.out.println(Arrays.toString(arr));
 		
-		//为了容量理解，我们把冒泡排序的演变过程，给大家展示
-		
+//		
+//		
 		//测试一下冒泡排序的速度O(n^2), 给80000个数据，测试
 		//创建要给80000个的随机的数组
 		int[] arr = new int[80000];
@@ -35,61 +39,13 @@ public class BubbleSort {
 		Date data2 = new Date();
 		String date2Str = simpleDateFormat.format(data2);
 		System.out.println("排序后的时间是=" + date2Str);
-		
-		//System.out.println("排序后");
-		//System.out.println(Arrays.toString(arr));
-		
-		
-		/*
-		
-		// 第二趟排序，就是将第二大的数排在倒数第二位
-		
-		for (int j = 0; j < arr.length - 1 - 1 ; j++) {
-			// 如果前面的数比后面的数大，则交换
-			if (arr[j] > arr[j + 1]) {
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
-		}
-		
-		System.out.println("第二趟排序后的数组");
-		System.out.println(Arrays.toString(arr));
-		
-		
-		// 第三趟排序，就是将第三大的数排在倒数第三位
-		
-		for (int j = 0; j < arr.length - 1 - 2; j++) {
-			// 如果前面的数比后面的数大，则交换
-			if (arr[j] > arr[j + 1]) {
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
-		}
 
-		System.out.println("第三趟排序后的数组");
-		System.out.println(Arrays.toString(arr));
-		
-		// 第四趟排序，就是将第4大的数排在倒数第4位
-
-		for (int j = 0; j < arr.length - 1 - 3; j++) {
-			// 如果前面的数比后面的数大，则交换
-			if (arr[j] > arr[j + 1]) {
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
-		}
-
-		System.out.println("第四趟排序后的数组");
-		System.out.println(Arrays.toString(arr)); */
 		
 	}
 	
 	// 将前面额冒泡排序算法，封装成一个方法
 	public static void bubbleSort(int[] arr) {
-		// 冒泡排序 的时间复杂度 O(n^2), 自己写出
+		// 冒泡排序 的时间复杂度 O(n^2)
 		int temp = 0; // 临时变量
 		boolean flag = false; // 标识变量，表示是否进行过交换
 		for (int i = 0; i < arr.length - 1; i++) {
@@ -103,8 +59,8 @@ public class BubbleSort {
 					arr[j + 1] = temp;
 				}
 			}
-			//System.out.println("第" + (i + 1) + "趟排序后的数组");
-			//System.out.println(Arrays.toString(arr));
+//			System.out.println("第" + (i + 1) + "趟排序后的数组");
+//			System.out.println(Arrays.toString(arr));
 
 			if (!flag) { // 在一趟排序中，一次交换都没有发生过
 				break;
@@ -112,6 +68,62 @@ public class BubbleSort {
 				flag = false; // 重置flag!!!, 进行下次判断
 			}
 		}
+	}
+	
+	public static void bubbleSort1(int[] arr) {
+		
+		 // 第一趟排序，就是将第一大的数排在倒数第一位
+		int temp = 0; // 临时变量
+		for (int j = 0; j < arr.length - 1 - 1 ; j++) {
+			// 如果前面的数比后面的数大，则交换
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+		
+		System.out.println("第一趟排序后的数组");
+		System.out.println(Arrays.toString(arr));
+		
+		// 第二趟排序，就是将第二大的数排在倒数第二位
+		for (int j = 0; j < arr.length - 1 - 1 ; j++) {
+			// 如果前面的数比后面的数大，则交换
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+		
+		System.out.println("第二趟排序后的数组");
+		System.out.println(Arrays.toString(arr));
+		
+		// 第三趟排序，就是将第三大的数排在倒数第三位
+		for (int j = 0; j < arr.length - 1 - 2; j++) {
+			// 如果前面的数比后面的数大，则交换
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+
+		System.out.println("第三趟排序后的数组");
+		System.out.println(Arrays.toString(arr));
+		
+		// 第四趟排序，就是将第4大的数排在倒数第4位
+		for (int j = 0; j < arr.length - 1 - 3; j++) {
+			// 如果前面的数比后面的数大，则交换
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+
+		System.out.println("第四趟排序后的数组");
+		System.out.println(Arrays.toString(arr)); 
 	}
 
 }
