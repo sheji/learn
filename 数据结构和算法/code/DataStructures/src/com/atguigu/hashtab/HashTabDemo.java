ac7db51e6460086fdb4a540e6e73f383b350866b
dll_glb <dll_glb@123.com>
2019-12-08 18:03:14 +0800

    数据结构和算法 哈希表

--- 数据结构和算法/code/DataStructures/src/com/atguigu/hashtab/HashTabDemo.java
+++ 数据结构和算法/code/DataStructures/src/com/atguigu/hashtab/HashTabDemo.java
@@ -8,42 +8,49 @@ public class HashTabDemo {
 		
 		//创建哈希表
 		HashTab hashTab = new HashTab(7);
+		hashTab.add(new Emp(1,"张三"));
+		hashTab.add(new Emp(2,"李四"));
+		hashTab.add(new Emp(3,"王五"));
 		
-		//写一个简单的菜单
-		String key = "";
-		Scanner scanner = new Scanner(System.in);
-		while(true) {
-			System.out.println("add:  添加雇员");
-			System.out.println("list: 显示雇员");
-			System.out.println("find: 查找雇员");
-			System.out.println("exit: 退出系统");
-			
-			key = scanner.next();
-			switch (key) {
-			case "add":
-				System.out.println("输入id");
-				int id = scanner.nextInt();
-				System.out.println("输入名字");
-				String name = scanner.next();
-				//创建 雇员
-				Emp emp = new Emp(id, name);
-				hashTab.add(emp);
-				break;
-			case "list":
-				hashTab.list();
-				break;
-			case "find":
-				System.out.println("请输入要查找的id");
-				id = scanner.nextInt();
-				hashTab.findEmpById(id);
-				break;
-			case "exit":
-				scanner.close();
-				System.exit(0);
-			default:
-				break;
-			}
-		}
+		hashTab.list();
+		
+		
+		
+//		//写一个简单的菜单
+//		String key = "";
+//		Scanner scanner = new Scanner(System.in);
+//		while(true) {
+//			System.out.println("add:  添加雇员");
+//			System.out.println("list: 显示雇员");
+//			System.out.println("find: 查找雇员");
+//			System.out.println("exit: 退出系统");
+//			
+//			key = scanner.next();
+//			switch (key) {
+//			case "add":
+//				System.out.println("输入id");
+//				int id = scanner.nextInt();
+//				System.out.println("输入名字");
+//				String name = scanner.next();
+//				//创建 雇员
+//				Emp emp = new Emp(id, name);
+//				hashTab.add(emp);
+//				break;
+//			case "list":
+//				hashTab.list();
+//				break;
+//			case "find":
+//				System.out.println("请输入要查找的id");
+//				id = scanner.nextInt();
+//				hashTab.findEmpById(id);
+//				break;
+//			case "exit":
+//				scanner.close();
+//				System.exit(0);
+//			default:
+//				break;
+//			}
+//		}
 		
 	}
 
