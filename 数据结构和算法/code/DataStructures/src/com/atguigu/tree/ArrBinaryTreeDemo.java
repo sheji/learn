c36695845149ce3f8da562f5ed0910566eb247d1
dll_glb <dll_glb@123.com>
2019-12-11 22:14:04 +0800

    数据结构和算法 树结构

--- 数据结构和算法/code/DataStructures/src/com/atguigu/tree/ArrBinaryTreeDemo.java
+++ 数据结构和算法/code/DataStructures/src/com/atguigu/tree/ArrBinaryTreeDemo.java
@@ -12,7 +12,6 @@ public class ArrBinaryTreeDemo {
 }
 
 //编写一个ArrayBinaryTree, 实现顺序存储二叉树遍历
-
 class ArrBinaryTree {
 	private int[] arr;//存储数据结点的数组
 
@@ -27,7 +26,6 @@ class ArrBinaryTree {
 	
 	//编写一个方法，完成顺序存储二叉树的前序遍历
 	/**
-	 * 
 	 * @param index 数组的下标 
 	 */
 	public void preOrder(int index) {
